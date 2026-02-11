cf42d27fd69bc142485482375388482c2dca3130
suryabhan8853 <25116cn137@glbitm.ac.in>
2026-02-11 10:54:05 +0530

    Add files via upload
    Two number are equal

---
+++ suryabhan.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main() {
+    int a,b;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    printf("Enter a number : ");
+    scanf("%d",&b);
+    if(a==b){
+        printf("Number is equall");
+    }
+    else {
+        printf("Number is not equall");
+    }
+    return 0;
+
+}
