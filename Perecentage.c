ebb5c598f6d1c72624f408f207b3146470934187
suryabhan8853 <25116cn137@glbitm.ac.in>
2026-02-14 00:32:09 +0530

    Add files via upload
    percentage of five subject

---
+++ Perecentage.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    float m1 = 93; // maths marks
+    float m2 = 89; // physics marks
+    float m3 = 85; // chem marks
+    float m4 = 90; // english marks
+    float m5 = 92; // hindi marks
+    float p = (m1 + m2 + m3 + m4 + m5)/5;
+    printf("%f", p);
+    return 0;
+
+}
