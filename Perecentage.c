d3eaf82f32305999783266229929a1ffab3dea96
suryabhan8853 <25116cn137@glbitm.ac.in>
2026-03-18 00:33:00 +0530

    Update Perecentage.c

--- Perecentage.c
+++ Perecentage.c
@@ -5,8 +5,11 @@ int main() {
     float m3 = 85; // chem marks
     float m4 = 90; // english marks
     float m5 = 92; // hindi marks
+    float sum;
+    sum = (m1 + m2 + m3 + m4 + m5);
+    printf("sum = %f\n",sum);
     float p = (m1 + m2 + m3 + m4 + m5)/5;
-    printf("%f", p);
+    printf("Percentage = %f", p);
     return 0;
 
 }
